04688054300da01c61c5ba030bc8e9809f7ce6fa
Brian Wangora <brianwangora@gmail.com>
2022-06-16 12:02:59 +0300

    lesson complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const print = (value) => console.log(value)
 
 // user scores
-const scores = [99 ,80, 77, 86, 50, 33, 76, 90, 54, 66]
+const scores = [99 ,4567 , 80, 8, 77, 86, 76, 7, 50, 33, 76, 90, 54, 66, 76, 9, 333, 21]
 
 // TODO: IDENTIFY THE TIME COMPLEXITY OF THE FOLLOWING ARRAY METHODS
 // 1: PUSH
@@ -43,28 +43,64 @@ const findValueLinear = (value) => {
 
 
 // TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LOGARITHMIC TIME COMPLEXITY (HAS BUG, TO BE FIXED)
-scores.sort()
-const findValueLog = (value, top, bottom) => {
-    let index = (-1)
+//value = 20, 90
+    // [100,20,23,55,76,90]
+    // [10,20,23] [55, 76, 90]
+    // find middle [3]
+    // check if middle is larger than value(20)
+    // middle in lower half [1]
+    // check if middle is larger than value(20)
+    // [10] [20,23]
+    // if value = middle => found, the value does not exist in array
+    // BINARY SEARCH ALGORITHM
 
-    let halfOps = (top + bottom) / 2
-    const half = parseInt(halfOps)
 
-    const valBeforeHalf = scores[half - 1]
-    if (value > valBeforeHalf) {
-        //top half
-        findValueLog(value, half - 1, 0)    
-    }else{
-        //bottom half
-        if (value === scores[half]) {
-            return half
+scores.sort((a, b) => a - b)
+print(scores)
+const findValueLog = (value) => {
+
+    let start = 0
+    let end = scores.length - 1
+
+    while (start <= end) {
+        
+        let middle = parseInt( (start + end) / 2)
+        // or let middle = Math.floor( (start + end) / 2)
+
+        if (scores[middle] === value) {
+            //return index if value equates
+            return middle
+        } else if (scores[middle] >= value) {
+            end = middle - 1
+        } else{
+            start = middle + 1
         }
-        findValueLog(value, scores.length - 1, half)
     }
 
-    return index
+    return -1
     // return the index of the value if present, return -1 if absent
 }
 
-print(`90 is at index: ${findValueLog(90, scores.length - 1, 0 )}`)
-print(`88 is at index: ${findValueLog(88, scores.length - 1, 0)}`)
+// recursive approach
+const searchLogRecursive = (value, start, end) => {
+
+    if(start > end) return -1
+
+    let middle = Math.trunc((start + end) / 2)
+
+    if(scores[middle] === value) return middle
+
+    if(scores[middle] > value) {
+        searchLogRecursive(value, start, middle - 1)
+    }else{
+        searchLogRecursive(value, middle + 1, end)
+    } 
+    return -1   
+}
+
+print(`90 is at index: ${findValueLog(90)}`)
+print(`88 is at index: ${findValueLog(88)}`)
+
+print(`808 is at index: ${searchLogRecursive(808, 0, scores.length - 1)}`)
+print(`90 is at index: ${searchLogRecursive(90, 0, scores.length - 1)}`)
+print(`76 is at index: ${searchLogRecursive(76, 0, scores.length - 1)}`)
